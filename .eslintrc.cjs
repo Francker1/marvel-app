ac72fa5ee7e1051dcd1dd46ffda92f985fd518e9
Ítalo Franco <italo.ravina@gmail.com>
2024-07-25 16:53:42 +0200

    feat: :art: Add Prettier and config lintern to code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
