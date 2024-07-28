8b8b63e2213e2052d446a4c74b13790eb0eeff9a
Ítalo Franco <italo.ravina@gmail.com>
2024-07-28 23:19:11 +0200

    feat: add search function and limit results because marvel api is too slow

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,36 +1,51 @@
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import './home-style.css';
-import charactersData from '../data/characters.json';
 import CharacterCard from '../components/CharacterCard';
-//import { type Character } from '../types';
 import { useFavorites } from '../context/FavoritesContext';
+import axios from 'axios';
+import { Character } from '../types';
+
+const apiKey = import.meta.env.VITE_MARVEL_API_KEY;
+const apiHash = import.meta.env.VITE_MARVEL_API_HASH;
+const apiBaseUrl = 'https://gateway.marvel.com/v1/public/characters';
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [characters, setCharacters] = useState<any[]>([]);
+  const [characters, setCharacters] = useState<Character[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
   const { isFavorite, addFavorite, removeFavorite } = useFavorites();
 
   useEffect(() => {
-    // fetch(
-    //   'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${apiKey}&hash=${apiHash}&limit=50',
-    // )
-    //   .then(async (res) => await res.json())
-    //   .then((res) => {
-    //     setCharacters(res.data.results);
-    //     setIsLoading(false);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
     fetchCharacters();
   }, []);
 
-  const fetchCharacters = async () => {
-    // Simulation API call because Marvel api is too slow
-    setCharacters(charactersData.data.results);
-    setIsLoading(false);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      fetchCharacters(searchTerm);
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
+
+  const fetchCharacters = async (nameStartsWith = '') => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(apiBaseUrl, {
+        params: {
+          apikey: apiKey,
+          ts: 1,
+          hash: apiHash,
+          limit: 25,
+          nameStartsWith: nameStartsWith || undefined,
+        },
+      });
+      setCharacters(response.data.data.results);
+    } catch (error) {
+      console.error('Error fetching characters:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
